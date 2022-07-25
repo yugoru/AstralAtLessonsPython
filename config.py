34d6c10eb3fa15b80391df235771c141dde6d4cb
gorshkova_ye <ya.baba.marta@gmail.com>
2022-07-25 12:15:18 +0300

    Добавила подключение к БД, но оно не работает

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-
 APP_BASE_URL = "http://localhost:8000/"
+
+DB_CONNECTION_PARAMS = {
+    "dbname": "online-wallet-fake",
+    "host": "localhost",
+    "user": "postgres",
+    "password": "postgres"
+}
--- tests/test_succsess_create_user.py
+++ tests/test_succsess_create_user.py
@@ -1,12 +1,22 @@
-from config import APP_BASE_URL
+import psycopg2
+
+from config import APP_BASE_URL, DB_CONNECTION_PARAMS
 from test_data.create_user import SCENARIO, ONLY_REQUIRED, checkLoginAfterCreate
 from app_driver.owf_http_client import OwfHttpClient
+from app_driver.user_repository import UserRepository
 
 
 class TestSuccessCreateUser:
 
     def setup(self):
         self.client = OwfHttpClient(APP_BASE_URL)
+        self.connection = psycopg2.connect(
+            dbname=DB_CONNECTION_PARAMS('dbname'),
+            user=DB_CONNECTION_PARAMS('user'),
+            password=DB_CONNECTION_PARAMS('password'),
+            host=DB_CONNECTION_PARAMS('host')
+        )
+        self.user_repository = UserRepository(self.connection)
 
     def test_register(self):
         register_response = self.client.register(SCENARIO)
@@ -21,3 +31,9 @@ class TestSuccessCreateUser:
         assert register_response.status_code == 200
         assert response_body["message"] == "Пользователь " + ONLY_REQUIRED['email'] + " успешно зарегистрирован"
         checkLoginAfterCreate(ONLY_REQUIRED)
+
+    def test_get_user_date(self):
+        self.user_repository.get_users()
+
+    def teardown(self):
+        self.connection.close()
